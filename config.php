f259370c2060e0af693d69a530e1be1dc7d38dc8
Taylor Allen <28820818+allenbybid@users.noreply.github.com>
2018-09-05 12:05:20 +0800

    YTB update
    YTB update

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyBbtztTaxJTF5e8ZG4bQQuu8_nBm_bdpU4');
+define('GJ_CODE', 'US');
+define('SITE_NAME', 'HW!');
+define('TITLENAME', 'HW!');
+define('EN2DEKEY', 'W-p{%e/3N>ruJi{Wor`08st;DyoD3Jm');
+define('EMAIL', '1299992@gmail.com');
+?>
